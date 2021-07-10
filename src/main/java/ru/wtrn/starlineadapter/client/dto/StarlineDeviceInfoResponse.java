bdf779d15b0d8dde84e11672bc56369fcfcee96a
Dmitry Romanov <me@wtrn.ru>
2021-07-10 12:17:21 +0300

    WIP StarlineDeviceInfoResponse

---
+++ src/main/java/ru/wtrn/starlineadapter/client/dto/StarlineDeviceInfoResponse.java
@@ -0,0 +1,28 @@
+package ru.wtrn.starlineadapter.client.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@SuppressWarnings("SpellCheckingInspection")
+public class StarlineDeviceInfoResponse {
+
+    Answer answer;
+
+    @Data
+    static class Answer {
+        List<Device> devices;
+    }
+
+    @Data
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    static class Device {
+        String alias;
+        String deviceId;
+        int ctemp;
+        int etemp;
+    }
+}
