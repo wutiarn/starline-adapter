102348dfa49eb383a3b7491d0c0abb4d876aa82d
Dmitry Romanov <me@wtrn.ru>
2021-07-10 10:35:47 +0300

    Init

---
+++ src/main/java/ru/wtrn/starlineadapter/StarlineAdapterApplication.java
@@ -0,0 +1,13 @@
+package ru.wtrn.starlineadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlineAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarlineAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/wtrn/starlineadapter/StarlineAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wtrn.starlineadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarlineAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
