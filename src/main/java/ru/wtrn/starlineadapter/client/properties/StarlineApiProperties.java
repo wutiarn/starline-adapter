30e79a4e5b5f50a8ec6aa1f1d54db596fba0f7ae
Dmitry Romanov <me@wtrn.ru>
2021-07-24 15:26:42 +0300

    WIP: Configure properties

--- src/main/java/ru/wtrn/starlineadapter/client/properties/StarlineApiProperties.java
+++ src/main/java/ru/wtrn/starlineadapter/client/properties/StarlineApiProperties.java
@@ -1,17 +1,13 @@
 package ru.wtrn.starlineadapter.client.properties;
 
-import lombok.Builder;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@Builder
-@ConfigurationProperties("ru.wtrn.starline-adapter")
+@ConfigurationProperties("ru.wtrn.starline-adapter.api")
 public class StarlineApiProperties {
     String baseUrl;
     String username;
     String password;
-
-    @Builder.Default
     String authCacheLocation = "starline-auth.txt";
 }
