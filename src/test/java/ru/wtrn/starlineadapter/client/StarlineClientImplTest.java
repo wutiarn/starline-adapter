1dd00529255853bfe8dfcca9e1172f4373f58e5f
Dmitry Romanov <me@wtrn.ru>
2021-08-03 18:57:59 +0300

    Verify wiremock calls in StarlineClientImplTest.testAuthInterceptorWorks

--- src/test/java/ru/wtrn/starlineadapter/client/StarlineClientImplTest.java
+++ src/test/java/ru/wtrn/starlineadapter/client/StarlineClientImplTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import ru.wtrn.starlineadapter.client.dto.StarlineLoginRequest;
 import ru.wtrn.starlineadapter.client.model.StarlineDevice;
 import ru.wtrn.starlineadapter.client.properties.StarlineApiProperties;
 import ru.wtrn.starlineadapter.support.BaseSpringBootTest;
@@ -24,12 +25,15 @@ class StarlineClientImplTest extends BaseSpringBootTest {
 
     private StarlineClientImpl starlineClient;
     private File authTempFile;
+    private final StarlineApiProperties properties = new StarlineApiProperties();
+
+    ObjectMapper objectMapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
 
     @BeforeEach
     @SneakyThrows
     void setup() {
-        StarlineApiProperties properties = new StarlineApiProperties();
-
         authTempFile = File.createTempFile("test-starline-auth", ".txt");
         authTempFile.deleteOnExit();
 
@@ -37,9 +41,6 @@ class StarlineClientImplTest extends BaseSpringBootTest {
         properties.setBaseUrl(String.format("%s/starline", wireMockServer.baseUrl()));
         properties.setUsername("test");
         properties.setPassword("testPassword");
-
-        ObjectMapper objectMapper = new ObjectMapper()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         starlineClient = new StarlineClientImpl(properties, objectMapper);
     }
@@ -71,11 +72,21 @@ class StarlineClientImplTest extends BaseSpringBootTest {
         List<StarlineDevice> devices = starlineClient.getDevices();
         Assertions.assertEquals("860920000000000", devices.get(0).getDeviceId());
 
+        String expectedCookie = "lang=ru; PHPSESSID=t39lmsvr6pqwerty633hmj9c68; userAgentId=b543aee857a543392c85c72bb3qwerty";
         String cachedAuth = Files.readString(authTempFile.toPath());
         Assertions.assertEquals(
-                "lang=ru; PHPSESSID=t39lmsvr6pqwerty633hmj9c68; userAgentId=b543aee857a543392c85c72bb3qwerty",
+                expectedCookie,
                 cachedAuth
         );
-    }
 
+        wireMockServer.verify(1, getRequestedFor(urlEqualTo("/starline/device"))
+                .withHeader(HttpHeaders.COOKIE, equalTo(expectedCookie)));
+
+        StarlineLoginRequest expectedLoginRequest = StarlineLoginRequest.builder()
+                .username(properties.getUsername())
+                .password(properties.getPassword())
+                .build();
+        wireMockServer.verify(1, postRequestedFor(urlEqualTo("/starline/rest/security/login"))
+                .withRequestBody(equalToJson(objectMapper.writeValueAsString(expectedLoginRequest))));
+    }
 }
